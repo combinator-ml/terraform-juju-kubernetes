648473375b93995dd2214c4c85a8e4bdb0bb8bb7
Luke Marsden <me@lukemarsden.net>
2021-06-06 22:14:14 +0100

    hacks

--- main.tf
+++ main.tf
@@ -25,7 +25,9 @@ resource "null_resource" "testfaster_vm" {
             export KUBECONFIG=$(pwd)/kubeconfig
             snap install juju --classic --channel=2.9/stable
             /snap/bin/juju add-k8s --client k8s
-            /snap/bin/juju bootstrap k8s
+            # Use clusterIP service type and then hopefully juju will
+            # port-forward to it https://github.com/juju/juju/pull/12512
+            /snap/bin/juju bootstrap k8s --config controller-service-type=cluster
             /snap/bin/juju deploy bundle.yaml
         EOT
     }
